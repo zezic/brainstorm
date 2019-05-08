529e65fbd7408d017f127f76834a5dde26d518c6
Sergey Ukolov <zezic51@yandex.ru>
2019-05-08 12:43:30 +0300

    Added a title.

--- src/Toolbar.vue
+++ src/Toolbar.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="brainstorm-toolbar">
-    <div class="slot left"><slot name='left'></slot></div>
-    <div class="slot middle"><slot name='middle'></slot></div>
-    <div class="slot right"><slot name='right'></slot></div>
+    <div class="toolbar-title">Brainstormer</div>
+    <div class="toolbar-content">
+      <div class="slot left"><slot name='left'></slot></div>
+      <div class="slot middle"><slot name='middle'></slot></div>
+      <div class="slot right"><slot name='right'></slot></div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +23,15 @@ export default {
   background-color: var(--brainstorm-accent-bg);
   color: var(--brainstorm-accent-fg);
   box-shadow: inset 0 -1px rgba(0, 0, 0, .1);
+}
+.brainstorm-toolbar .toolbar-title {
+  text-align: center;
+  font-weight: 600;
+  font-size: 18px;
+  box-shadow: inset 0 -1px rgba(0, 0, 0, .1);
+  padding: 20px;
+}
+.brainstorm-toolbar .toolbar-content {
   display: flex;
   flex-direction: row;
   padding: 10px;
